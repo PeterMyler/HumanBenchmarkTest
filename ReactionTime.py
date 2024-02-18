f122d96f41e3fd101c724f9cc455fc258ab1abee
PeterTheCool <petermyler100@gmail.com>
2024-02-18 18:37:44 +0000

    d3

--- ReactionTime.py
+++ ReactionTime.py
@@ -3,6 +3,8 @@ from mouse import click
 from time import sleep
 moveTo(500, 620)
 
+# continuously checks if the pixel at (952, 280) is green
+# right-clicks twice if it is
 while True:
     if pixelMatchesColor(952, 280, (81, 218, 114)):
         click()

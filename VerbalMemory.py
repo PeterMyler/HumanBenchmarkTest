5208ed6afb9c667a6437e0ecc7bc611062b77baa
PeterTheCool <petermyler100@gmail.com>
2024-04-02 18:33:07 +0100

    init

--- VerbalMemory.py
+++ VerbalMemory.py
@@ -5,25 +5,31 @@ from time import sleep
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 sleep(1)
-print("running")
+# 125% zoom, not full-screen, no bookmarks
 
-words = []
+# (x, y) screen coords
+seen_pos = (868, 603)
+new_pos = (1036, 603)
+# (x, y, w, h)
+word_region = (550, 435, 800, 90)
+
+words = set()
 unique_words = 0
 
-while not is_pressed("escape"):
-    word_img = screenshot(region=(2470-1920, 460, 807, 78))
-    word_string = pytesseract.image_to_string(word_img).replace("\n", "").replace(" ", "")
-
-    # if prev_word == word_string: continue
+while not is_pressed("p"):
+    word_img = screenshot(region=word_region)
+    word_string = pytesseract.image_to_string(word_img)
+    word_string = word_string.strip().replace(" ", "").lower()
 
     if word_string in words:
-        click(868, 603)  # click "seen"
+        click(*seen_pos)  # click "seen"
     else:
-        words.append(word_string)
-        click(1036, 603)  # click new
+        words.add(word_string)
+        click(*new_pos)  # click "new"
         unique_words += 1
 
-        if unique_words % 100 == 0: print(unique_words, "unique words reached")
+        if unique_words % 100 == 0:
+            print(unique_words, "unique words reached")
 
     sleep(0.02)
 
